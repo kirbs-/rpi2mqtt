99a92806e408fc5acb6440f5dbf71f19007b575f
kirbs- <kirbycm@gmail.com>
2019-11-24 15:29:12 -0500

    Added switch setup from YAML config.

--- rpi2mqtt/event_loop.py
+++ rpi2mqtt/event_loop.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 from rpi2mqtt.binary import *
 from rpi2mqtt.temperature import *
 from rpi2mqtt.ibeacon import Scanner
+from rpi2mqtt.switch import Switch
 import time
 import rpi2mqtt.mqtt as mqtt
 from beacontools import BeaconScanner, IBeaconFilter
@@ -17,6 +18,8 @@ def main():
             s = DHT(sensor.pin, sensor.topic, sensor.name, 'sensor', sensor.type)
         elif sensor.type == 'ibeacon':
             s = Scanner(sensor.name, sensor.topic, sensor.uuid, sensor.away_timeout)
+        elif sensor.type == 'switch':
+            s = Switch(sensor.pin, sensor.name, sensor.topic)
 
         sensor_list.append(s)
 
