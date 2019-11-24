9263c61007b834abc45ca5879f29af8c7e1698e7
kirbs- <kirbycm@gmail.com>
2019-11-24 15:18:47 -0500

    Added mqtt subscribe.

--- rpi2mqtt/mqtt.py
+++ rpi2mqtt/mqtt.py
@@ -1,4 +1,5 @@
 import paho.mqtt.publish as mqtt
+import paho.mqtt.subscribe as mqtt_sub
 from rpi2mqtt.config import config
 import traceback
 
@@ -13,3 +14,9 @@ def publish(topic, payload, cnt=1):
         traceback.print_exc()
         cnt += 1
         publish(topic, payload, cnt)
+
+
+def subscribe(topic, callback):
+    mqtt_sub.callback(callback, topic, hostname=config.mqtt.host, port=config.mqtt.port,
+                        auth={'username': config.mqtt.username, 'password': config.mqtt.password},
+                        tls={'ca_certs': config.mqtt.ca_cert})
