b245d7ff12ec58c4bfeca910d1c9655579f5ea02
kirbs- <kirbycm@gmail.com>
2021-01-27 16:18:04 -0500

    Bug fix.

--- rpi2mqtt/config.py
+++ rpi2mqtt/config.py
@@ -37,22 +37,22 @@ class Config(DotMap):
             level = logging.ERROR
         logging.basicConfig(level=level, stream=sys.stdout)
 
-    @staticmethod
-    def generate_config(config_filename):
-        with open(config_filename, 'w') as f: 
-            default_config = {
-                "logging": 'warn',
-                'polling_interval': 300,
-                'mqtt': {
-                    'host': 'hostname',
-                    'port': 1883,
-                    'username': 'mqtt_user',
-                    'password': 'mqtt_password',
-                    'retries': 3,
-                    'ca_cert': '/path/to/certificate'
-                },
-                'sensors': [
-                    {'name': 'sensor_name', 'type': 'sensor_type', 'pin': 'gpio pin number'}
-                ]}
-            f.write(yaml.dump(default_config))
+
+def generate_config(config_filename):
+    with open(config_filename, 'w') as f: 
+        default_config = {
+            "logging": 'warn',
+            'polling_interval': 300,
+            'mqtt': {
+                'host': 'hostname',
+                'port': 1883,
+                'username': 'mqtt_user',
+                'password': 'mqtt_password',
+                'retries': 3,
+                'ca_cert': '/path/to/certificate'
+            },
+            'sensors': [
+                {'name': 'sensor_name', 'type': 'sensor_type', 'pin': 'gpio pin number'}
+            ]}
+        f.write(yaml.dump(default_config))
 
