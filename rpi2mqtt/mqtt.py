73f7753a75302e21d48541a8fda7e6e5f1c98169
kirbs- <kirbycm@gmail.com>
2021-02-02 19:16:16 -0500

    Added pongable logging

--- rpi2mqtt/mqtt.py
+++ rpi2mqtt/mqtt.py
@@ -85,6 +85,7 @@ class MQTT():
     def pongable(func):
         # def decorator_wrapper(func):
         def wrapper(client, userdata, message):
+            logging.debug('Received message {} on topic {}'.format(message, message.topic))
             payload = message.payload.decode()
             if payload == 'ping':
                 MQTT.publish(message.topic, "pong") 
